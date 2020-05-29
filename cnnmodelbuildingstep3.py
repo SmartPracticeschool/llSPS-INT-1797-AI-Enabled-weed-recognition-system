6784ad59fd0236d1fdc8bd2c89cd3280b51b7517
Naveen-oops <59889378+Naveen-oops@users.noreply.github.com>
2020-05-29 17:56:28 +0530

    Update and rename weeddettector.py to cnnmodelbuildingstep3.py

--- cnnmodelbuildingstep3.py
+++ cnnmodelbuildingstep3.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 24 09:19:26 2020
+CNN model building step3
 
-@author: sys
-"""
 from keras.preprocessing.image import ImageDataGenerator
 train_data=ImageDataGenerator(rescale=1./255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
 test_data=ImageDataGenerator(rescale=1./255)
